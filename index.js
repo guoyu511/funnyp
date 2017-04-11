3e4c5ec1fe2fd39408586ae817c9e8b9f5932545
GuoYu <guoyu.511@gmail.com>
2017-04-11 18:06:58 +0800

    remove code

--- index.js
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env node
-
-'use strict';
-
-var opt = require('node-getopt').create([
-        ['p', '=', 'Web UI bind port']
-    ]).bindHelp().parseSystem();
-
-require('./lib/startup')(opt['options']['p']);
